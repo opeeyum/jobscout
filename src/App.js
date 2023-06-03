af31412ce3b3eea76a5c4c17161e4422ad3ed068
sonal <sonalsingh4102@gmail.com>
2023-06-03 22:50:54 +0530

    route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import AppHeader from "./components/sections/AppHeader";
 import BodySection from "./components/sections/BodySection";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import JobDetailSection from "./components/sections/JobDetailSection";
 
 function App() {
 	return (
 		<>
 			<AppHeader />
-      <BodySection/>
+
+			<Router>
+				<Routes>
+					<Route path="/" element={<BodySection />} />
+					<Route path="/details" element={<JobDetailSection />} />
+				</Routes>
+			</Router>
 		</>
 	);
 }
