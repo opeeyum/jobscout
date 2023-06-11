821ad1b2b789f3d8b4bf9f98b2b06676c5846467
sonal <sonalsingh4102@gmail.com>
2023-06-11 14:11:38 +0530

    profile

--- src/components/sections/AppHeader.js
+++ src/components/sections/AppHeader.js
@@ -1,10 +1,30 @@
-import { AppBar, Box, Button, Toolbar } from "@mui/material";
+import {
+	AppBar,
+	Avatar,
+	Box,
+	Button,
+	Divider,
+	IconButton,
+	ListItemIcon,
+	Menu,
+	MenuItem,
+	Toolbar,
+	Tooltip,
+} from "@mui/material";
 import React from "react";
 
 import Logo from "../common/Icons/Logo";
-import { Login } from "@mui/icons-material";
+import { Login, Logout, PersonAdd, Settings } from "@mui/icons-material";
 
 function AppHeader() {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 	return (
 		<>
 			<AppBar position="sticky" sx={{ backgroundColor: "#537188" }}>
@@ -34,13 +54,101 @@ function AppHeader() {
 								textTransform: "capitalize",
 								transition: "background-color 0.5s ease",
 								"&:hover": {
-									backgroundColor: "#abc1d3",
+								 backgroundColor: "#abc1d3",
+									// backgroundColor: "#bdbdbd",
+									// color:"white"
 								},
+								height: "42px",
+								fontSize: "16px",
 							}}
 							endIcon={<Login />}
 						>
 							Login
 						</Button>
+
+						<Tooltip title="Account settings">
+							<IconButton
+								onClick={handleClick}
+								size="small"
+								sx={{ ml: 2 }}
+								// aria-controls={open ? 'account-menu' : undefined}
+								aria-haspopup="true"
+								// aria-expanded={open ? 'true' : undefined}
+							>
+								<Avatar sx={{ width: 42, height: 42 }}>
+									T
+								</Avatar>
+							</IconButton>
+						</Tooltip>
+
+						<Menu
+							anchorEl={anchorEl}
+							id="account-menu"
+							open={open}
+							onClose={handleClose}
+							onClick={handleClose}
+							PaperProps={{
+								elevation: 0,
+								sx: {
+									overflow: "visible",
+									filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+									mt: 1.5,
+									"& .MuiAvatar-root": {
+										width: 32,
+										height: 32,
+										ml: -0.5,
+										mr: 1,
+									},
+									"&:before": {
+										content: '""',
+										display: "block",
+										position: "absolute",
+										top: 0,
+										right: 14,
+										width: 10,
+										height: 10,
+										bgcolor: "background.paper",
+										transform:
+											"translateY(-50%) rotate(45deg)",
+										zIndex: 0,
+									},
+								},
+							}}
+							transformOrigin={{
+								horizontal: "right",
+								vertical: "top",
+							}}
+							anchorOrigin={{
+								horizontal: "right",
+								vertical: "bottom",
+							}}
+						>
+							<MenuItem onClick={handleClose}>
+								<Avatar /> Profile
+							</MenuItem>
+							<MenuItem onClick={handleClose}>
+								<Avatar /> My account
+							</MenuItem>
+							<Divider />
+							<MenuItem onClick={handleClose}>
+								<ListItemIcon>
+									<PersonAdd fontSize="small" />
+								</ListItemIcon>
+								Add another account
+							</MenuItem>
+							<MenuItem onClick={handleClose}>
+								<ListItemIcon>
+									<Settings fontSize="small" />
+								</ListItemIcon>
+								Settings
+							</MenuItem>
+							<MenuItem onClick={handleClose}>
+								<ListItemIcon>
+									<Logout fontSize="small" />
+								</ListItemIcon>
+								Logout
+							</MenuItem>
+						</Menu>
 					</Box>
 				</Toolbar>
 			</AppBar>
